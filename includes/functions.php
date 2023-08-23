5b5b8e14e1fe658e3461dc39d20f348afdb91e3a
frenziecodes <frenziecodes@gmail.com>
2023-08-23 14:52:22 +0300

    update

--- includes/functions.php
+++ includes/functions.php
@@ -22,59 +22,59 @@ function user_column_manager_register_menus() {
         'Manage Custom Columns',
         'Manage Columns',
         'manage_options',
-        'user-column-manager-manage-columns',
-        'user_column_manager_manage_columns_page'
+        'user_column_manager_settings',
+        'user_column_manager_settings_page'
     );
 
     // Submenu: Customize Custom Columns
-    add_submenu_page(
-        'user-column-manager',
-        'Customize Custom Columns',
-        'Customize Columns',
-        'manage_options',
-        'user_column_manager_customize_columns',
-        'user_column_manager_customize_columns_page'
-    );
+    // add_submenu_page(
+    //     'user-column-manager',
+    //     'Customize Custom Columns',
+    //     'Customize Columns',
+    //     'manage_options',
+    //     'user_column_manager_customize_columns',
+    //     'user_column_manager_customize_columns_page'
+    // );
 	
     // Submenu: import/export data
-    add_submenu_page(
-        'user-column-manager',
-        'Import/Export',
-        'Import/Export',
-        'manage_options',
-        'user_column_manager_import_export',
-        'user_column_manager_import_export_page'
-    );
+    // add_submenu_page(
+    //     'user-column-manager',
+    //     'Import/Export',
+    //     'Import/Export',
+    //     'manage_options',
+    //     'user_column_manager_import_export',
+    //     'user_column_manager_import_export_page'
+    // );
 
 	// Submenu: Sorting and Filtering / User Grouping
-	add_submenu_page(
-        'user-column-manager',
-        'Sorting and Grouping',
-        'Sorting & Grouping',
-        'manage_options',
-        'user_column_manager_sorting_grouping',
-        'user_column_manager_sorting_grouping_page'
-    );
+	// add_submenu_page(
+    //     'user-column-manager',
+    //     'Sorting and Grouping',
+    //     'Sorting & Grouping',
+    //     'manage_options',
+    //     'user_column_manager_sorting_grouping',
+    //     'user_column_manager_sorting_grouping_page'
+    // );
 
     // Submenu: premium
-    add_submenu_page(
-        'user-column-manager',
-        'Upgrade To Premium',
-        'Upgrade To Premium',
-        'manage_options',
-        'user_column_manager_premium',
-        'user_column_manager_premium_page'
-    );
+    // add_submenu_page(
+    //     'user-column-manager',
+    //     'Upgrade To Premium',
+    //     'Upgrade To Premium',
+    //     'manage_options',
+    //     'user_column_manager_premium',
+    //     'user_column_manager_premium_page'
+    // );
 
     // Submenu: support
-    add_submenu_page(
-        'user-column-manager',
-        'Help and support',
-        'Help and support',
-        'manage_options',
-        'user_column_manager_support',
-        'user_column_manager_support_page'
-    );
+    // add_submenu_page(
+    //     'user-column-manager',
+    //     'Help and support',
+    //     'Help and support',
+    //     'manage_options',
+    //     'user_column_manager_support',
+    //     'user_column_manager_support_page'
+    // );
 
     add_action('admin_print_styles', 'user_column_manager_add_submenu_icons');
 }
@@ -109,7 +109,7 @@ function user_column_manager_dashboard_page() {
                 <h2>Quick Actions</h2>
                 <ul class="quick-actions">
                     <li>
-                        <a href="<?php echo admin_url('admin.php?page=user-column-manager-manage-columns'); ?>">
+                        <a href="<?php echo admin_url('admin.php?page=user_column_manager_settings'); ?>">
                             <span class="dashicons dashicons-admin-settings"></span>
                             Manage Custom Columns
                         </a>
@@ -134,11 +134,133 @@ function user_column_manager_dashboard_page() {
     <?php
 }
 
-
-function user_column_manager_manage_columns_page() {
-    // Create, edit, and delete custom columns
-    // Reorder custom columns using drag-and-drop
+/**
+ * Add custom columns to the user list
+ *
+ * @param array $columns The existing columns in the user list.
+ * @return array The updated columns with custom ones.
+ */
+function user_column_manager_add_custom_columns( $columns ) {
+	$custom_columns = get_option( 'user_column_manager_columns', '' );
+	if ( ! empty( $custom_columns ) ) {
+		$custom_column_labels = explode( ',', $custom_columns );
+		$new_columns = array();
+		foreach ( $custom_column_labels as $label ) {
+			$column_key = sanitize_key( trim( $label ) );
+			$new_columns[ $column_key ] = $label;
+		}
+		$posts_index = array_search( 'posts', array_keys( $columns ) );
+		$before_posts = array_slice( $columns, 0, $posts_index, true );
+		$after_posts = array_slice( $columns, $posts_index, null, true );
+		$columns = array_merge( $before_posts, $new_columns, $after_posts );
+	}
+	return $columns;
 }
+add_filter( 'manage_users_columns', 'user_column_manager_add_custom_columns' );
+
+
+/**
+ * Display data in custom column
+ *
+ * @param string $value The default value to display.
+ * @param string $column_name The name of the column.
+ * @param int    $user_id The ID of the user.
+ * @return string The data to display in the custom column.
+ */
+function user_column_manager_show_user_column_data( $value, $column_name, $user_id ) {
+	$custom_columns = get_option( 'user_column_manager_columns', '' );
+	if ( ! empty( $custom_columns ) ) {
+		$custom_column_labels = explode( ',', $custom_columns );
+		foreach ( $custom_column_labels as $label ) {
+			$column_key = sanitize_key( trim( $label ) );
+			if ( $column_key === $column_name ) {
+				$additional_data = get_user_meta( $user_id, 'user_column_manager_additional_data_' . $column_key, true );
+
+				// Check if the value is empty, and display "-" if it is.
+				if ( empty( $additional_data ) ) {
+					return '-';
+				}
+
+				return $additional_data;
+			}
+		}
+	}
+
+	return $value;
+}
+add_filter( 'manage_users_custom_column', 'user_column_manager_show_user_column_data', 10, 3 );
+
+
+/**
+ * Add additional column fields to "Add New User" page
+ */
+function user_column_manager_add_new_user_fields() {
+	$custom_columns = get_option( 'user_column_manager_columns', '' );
+	if ( ! empty( $custom_columns ) ) {
+		$custom_column_labels = explode( ',', $custom_columns );
+		foreach ( $custom_column_labels as $label ) {
+			$column_key = sanitize_key( trim( $label ) );
+			?>
+			<table class="form-table">
+				<tr>
+					<th><label for="<?php echo esc_attr( $column_key ); ?>"><?php echo esc_html( $label ); ?>:</label></th>
+					<td>
+						<input type="text" name="<?php echo esc_attr( $column_key ); ?>" id="<?php echo esc_attr( $column_key ); ?>" value="" class="regular-text" />
+					</td>
+				</tr>
+			</table>
+			<?php
+		}
+	}
+}
+
+add_action( 'user_new_form', 'user_column_manager_add_new_user_fields' );
+add_action( 'edit_user_profile', 'user_column_manager_add_new_user_fields' );
+add_action( 'show_user_profile', 'user_column_manager_add_new_user_fields' );
+
+/**
+ * Save additional data when a new user is registered
+ *
+ * @param int $user_id The ID of the newly registered user.
+ */
+function user_column_manager_save_additional_data_on_registration( $user_id ) {
+	$custom_columns = get_option( 'user_column_manager_columns', '' );
+	if ( ! empty( $custom_columns ) ) {
+		$custom_column_labels = explode( ',', $custom_columns );
+		foreach ( $custom_column_labels as $label ) {
+			$column_key = sanitize_key( trim( $label ) );
+			if ( isset( $_POST[ $column_key ] ) ) {
+				$additional_data = sanitize_text_field( $_POST[ $column_key ] );
+				update_user_meta( $user_id, 'user_column_manager_additional_data_' . $column_key, $additional_data );
+			}
+		}
+	}
+}
+add_action( 'user_register', 'user_column_manager_save_additional_data_on_registration' );
+
+
+/**
+ * Save additional data when editing a user profile
+ *
+ * @param int $user_id The ID of the user being updated.
+ */
+function user_column_manager_save_additional_data_on_profile_update( $user_id ) {
+	$custom_columns = get_option( 'user_column_manager_columns', '' );
+	if ( ! empty( $custom_columns ) ) {
+		$custom_column_labels = explode( ',', $custom_columns );
+		foreach ( $custom_column_labels as $label ) {
+			$column_key = sanitize_key( trim( $label ) );
+			if ( isset( $_POST[ $column_key ] ) ) {
+				$additional_data = sanitize_text_field( $_POST[ $column_key ] );
+				update_user_meta( $user_id, 'user_column_manager_additional_data_' . $column_key, $additional_data );
+			}
+		}
+	}
+}
+
+add_action( 'personal_options_update', 'user_column_manager_save_additional_data_on_profile_update' );
+add_action( 'edit_user_profile_update', 'user_column_manager_save_additional_data_on_profile_update' );
+
 function user_column_manager_customize_columns_page() {
     // Customize the styling and formatting of custom columns
 	// Apply fonts, colors, alignment, and other visual settings
--- user-column-manager.php
+++ user-column-manager.php
@@ -15,6 +15,13 @@
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
 
+// Enqueue CSS and JavaScript files.
+function user_column_manager_enqueue_scripts() {
+    wp_enqueue_style( 'custom-columns-style', plugin_dir_url( __FILE__ ) . 'assets/css/usc-main.css', array(), '0.1.0' );
+    wp_enqueue_script( 'custom-columns-script', plugin_dir_url( __FILE__ ) . 'assets/js/usc-main.js', array( 'jquery' ), '0.1.0', true );
+}
+add_action( 'admin_enqueue_scripts', 'user_column_manager_enqueue_scripts' );
+
 require_once plugin_dir_path( __FILE__ ) . 'includes/functions.php';
 
 
@@ -22,154 +29,43 @@ require_once plugin_dir_path( __FILE__ ) . 'includes/functions.php';
  * Settings page content
  */
 function user_column_manager_settings_page() {
-	if ( ! current_user_can( 'manage_options' ) ) {
-		return;
-	}
+    if (!current_user_can('manage_options')) {
+        return;
+    }
 
-	// Save custom columns on form submission.
-	if ( isset( $_POST['user_column_manager_columns'] ) ) {
-		$columns = sanitize_text_field( $_POST['user_column_manager_columns'] );
-		update_option( 'user_column_manager_columns', $columns );
-		echo '<div class="notice notice-success is-dismissible"><p>Custom columns have been added successfully.</p></div>';
-	}
+    // Save custom columns on form submission.
+    if (isset($_POST['user_column_manager_columns'])) {
+        $columns = sanitize_text_field($_POST['user_column_manager_columns']);
+        update_option('user_column_manager_columns', $columns);
+        echo '<div class="notice notice-success is-dismissible"><p>Custom columns have been added successfully.</p></div>';
+    }
 
-	// Get existing custom columns.
-	$existing_columns = get_option( 'user_column_manager_columns', '' );
+    // Get existing custom columns.
+    $existing_columns = get_option('user_column_manager_columns', '');
 
-	?>
-	<div class="wrap">
-		<h1>User Column Manager</h1>
-		<form method="post">
-			<label for="user_column_manager_columns">Enter column names separated by commas:</label>
-			<input type="text" name="user_column_manager_columns" id="user_column_manager_columns" value="<?php echo esc_attr( $existing_columns ); ?>" class="regular-text" />
-			<?php submit_button( 'Save Columns', 'primary', 'user_column_manager_save_columns' ); ?>
-		</form>
-	</div>
-	<?php
+    ?>
+    <div class="wrap">
+        <h1>User Column Manager</h1>
+        <form method="post">
+            <label for="user_column_manager_columns">Enter column names separated by commas:</label>
+            <input type="text" name="user_column_manager_columns" id="user_column_manager_columns"
+                   value="<?php echo esc_attr($existing_columns); ?>" class="regular-text"/>
+            <?php submit_button('Save Columns', 'primary', 'user_column_manager_save_columns'); ?>
+        </form>
+        <?php
+        if (!empty($existing_columns)) {
+            echo '<h2>Custom Columns</h2>';
+            echo '<p>Drag and drop column to reorder</p>';
+            echo '<div id="custom-columns-list">';
+            $custom_column_labels = explode(',', $existing_columns);
+            foreach ($custom_column_labels as $label) {
+                $column_key = sanitize_key(trim($label));
+                echo '<div class="custom-column-item" data-column-key="' . esc_attr($column_key) . '">' . esc_html($label) . '<span class="delete-column">Delete</span></div>';
+            }
+            echo '</div>';
+        }
+        ?>
+    </div>
+    <?php
 }
 
-/**
- * Add custom columns to the user list
- *
- * @param array $columns The existing columns in the user list.
- * @return array The updated columns with custom ones.
- */
-function user_column_manager_add_custom_columns( $columns ) {
-	$custom_columns = get_option( 'user_column_manager_columns', '' );
-	if ( ! empty( $custom_columns ) ) {
-		$custom_column_labels = explode( ',', $custom_columns );
-		$new_columns = array();
-		foreach ( $custom_column_labels as $label ) {
-			$column_key = sanitize_key( trim( $label ) );
-			$new_columns[ $column_key ] = $label;
-		}
-		$posts_index = array_search( 'posts', array_keys( $columns ) );
-		$before_posts = array_slice( $columns, 0, $posts_index, true );
-		$after_posts = array_slice( $columns, $posts_index, null, true );
-		$columns = array_merge( $before_posts, $new_columns, $after_posts );
-	}
-	return $columns;
-}
-add_filter( 'manage_users_columns', 'user_column_manager_add_custom_columns' );
-
-/**
- * Display data in custom column
- *
- * @param string $value The default value to display.
- * @param string $column_name The name of the column.
- * @param int    $user_id The ID of the user.
- * @return string The data to display in the custom column.
- */
-function user_column_manager_show_user_column_data( $value, $column_name, $user_id ) {
-	$custom_columns = get_option( 'user_column_manager_columns', '' );
-	if ( ! empty( $custom_columns ) ) {
-		$custom_column_labels = explode( ',', $custom_columns );
-		foreach ( $custom_column_labels as $label ) {
-			$column_key = sanitize_key( trim( $label ) );
-			if ( $column_key === $column_name ) {
-				$additional_data = get_user_meta( $user_id, 'user_column_manager_additional_data_' . $column_key, true );
-
-				// Check if the value is empty, and display "-" if it is.
-				if ( empty( $additional_data ) ) {
-					return '-';
-				}
-
-				return $additional_data;
-			}
-		}
-	}
-
-	return $value;
-}
-add_filter( 'manage_users_custom_column', 'user_column_manager_show_user_column_data', 10, 3 );
-
-
-/**
- * Add additional column fields to "Add New User" page
- */
-function user_column_manager_add_new_user_fields() {
-	$custom_columns = get_option( 'user_column_manager_columns', '' );
-	if ( ! empty( $custom_columns ) ) {
-		$custom_column_labels = explode( ',', $custom_columns );
-		foreach ( $custom_column_labels as $label ) {
-			$column_key = sanitize_key( trim( $label ) );
-			?>
-			<table class="form-table">
-				<tr>
-					<th><label for="<?php echo esc_attr( $column_key ); ?>"><?php echo esc_html( $label ); ?>:</label></th>
-					<td>
-						<input type="text" name="<?php echo esc_attr( $column_key ); ?>" id="<?php echo esc_attr( $column_key ); ?>" value="" class="regular-text" />
-					</td>
-				</tr>
-			</table>
-			<?php
-		}
-	}
-}
-
-add_action( 'user_new_form', 'user_column_manager_add_new_user_fields' );
-add_action( 'edit_user_profile', 'user_column_manager_add_new_user_fields' );
-add_action( 'show_user_profile', 'user_column_manager_add_new_user_fields' );
-
-/**
- * Save additional data when a new user is registered
- *
- * @param int $user_id The ID of the newly registered user.
- */
-function user_column_manager_save_additional_data_on_registration( $user_id ) {
-	$custom_columns = get_option( 'user_column_manager_columns', '' );
-	if ( ! empty( $custom_columns ) ) {
-		$custom_column_labels = explode( ',', $custom_columns );
-		foreach ( $custom_column_labels as $label ) {
-			$column_key = sanitize_key( trim( $label ) );
-			if ( isset( $_POST[ $column_key ] ) ) {
-				$additional_data = sanitize_text_field( $_POST[ $column_key ] );
-				update_user_meta( $user_id, 'user_column_manager_additional_data_' . $column_key, $additional_data );
-			}
-		}
-	}
-}
-add_action( 'user_register', 'user_column_manager_save_additional_data_on_registration' );
-
-
-/**
- * Save additional data when editing a user profile
- *
- * @param int $user_id The ID of the user being updated.
- */
-function user_column_manager_save_additional_data_on_profile_update( $user_id ) {
-	$custom_columns = get_option( 'user_column_manager_columns', '' );
-	if ( ! empty( $custom_columns ) ) {
-		$custom_column_labels = explode( ',', $custom_columns );
-		foreach ( $custom_column_labels as $label ) {
-			$column_key = sanitize_key( trim( $label ) );
-			if ( isset( $_POST[ $column_key ] ) ) {
-				$additional_data = sanitize_text_field( $_POST[ $column_key ] );
-				update_user_meta( $user_id, 'user_column_manager_additional_data_' . $column_key, $additional_data );
-			}
-		}
-	}
-}
-
-add_action( 'personal_options_update', 'user_column_manager_save_additional_data_on_profile_update' );
-add_action( 'edit_user_profile_update', 'user_column_manager_save_additional_data_on_profile_update' );
