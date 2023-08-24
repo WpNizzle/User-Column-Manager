23bbfd6089d9c9f1cde76b1371bd791b3477bc52
Frenziecodes <coderflame3@gmail.com>
2023-08-24 13:06:16 +0300

    meet more wpcs

--- user-column-manager.php
+++ user-column-manager.php
@@ -15,12 +15,15 @@
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
 
-// Enqueue CSS and JavaScript files..
+/**
+ * Enqueue CSS and JavaScript files for User Column Manager.
+ */
 function user_column_manager_enqueue_scripts() {
-    wp_enqueue_style( 'custom-columns-style', plugin_dir_url( __FILE__ ) . 'assets/css/usc-main.css', array(), '0.1.0' );
-    wp_enqueue_script( 'custom-columns-script', plugin_dir_url( __FILE__ ) . 'assets/js/usc-main.js', array( 'jquery' ), '0.1.0', true );
+	wp_enqueue_style( 'custom-columns-style', plugin_dir_url( __FILE__ ) . 'assets/css/usc-main.css', array(), '0.1.0' );
+	wp_enqueue_script( 'custom-columns-script', plugin_dir_url( __FILE__ ) . 'assets/js/usc-main.js', array( 'jquery' ), '0.1.0', true );
 }
 add_action( 'admin_enqueue_scripts', 'user_column_manager_enqueue_scripts' );
+
 
 require_once plugin_dir_path( __FILE__ ) . 'includes/functions.php';
 
